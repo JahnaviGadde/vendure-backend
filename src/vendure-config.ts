0a2d9135923d5ba9050adcbdddfeb146d52a07d8
Jahnavi Gadde <96720036+JahnaviGadde@users.noreply.github.com>
2025-06-15 22:47:53 +0530

    trying to fix database issue

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -50,7 +50,7 @@ export const config: VendureConfig = {
     dbConnectionOptions: {
         type: 'postgres',
         // See the README.md "Migrations" section for an explanation of
-        // hello the `synchronize` and `migrations` options.
+        // hello hi the `synchronize` and `migrations` options.
         synchronize: process.env.DB_SYNCHRONIZE === 'true',
         migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
         logging: false,
